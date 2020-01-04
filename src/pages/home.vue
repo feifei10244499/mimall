e264fa37f0bf35d67cbd34407792d3a5bc2e6971
feifei <feifei10244499@qq.com>
2020-01-04 22:21:29 +0800

    init shop study

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <NavHeader></NavHeader>
+    <router-view></router-view>
+    <NavFooter></NavFooter>
+</template>
+
+<script>
+    import NavFooter from "../components/NavFooter";
+    export default {
+        name: "home",
+        components: {NavFooter}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/order.vue
@@ -0,0 +1,15 @@
+<template>
+    <NavHeader></NavHeader>
+    <router-view></router-view>
+    <NavFooter></NavFooter>
+</template>
+
+<script>
+    export default {
+        name: "order"
+    }
+</script>
+
+<style scoped>
+
+</style>
