3b4f2c00741f92263ac93a5cc749984a4179b7d4
feifei <feifei10244499@qq.com>
2020-01-05 21:53:40 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,25 @@
 </template>
 
 <script>
+import storage from "./storage/index";
 export default {
-  name: 'app',
-  components: {
-
+  name: "app",
+  components: {},
+  mounted() {
+    //Storage测试
+    //mall   对应的value   {"user":{"abc":{"a":1},"abcd":{"aa":2}}}
+    // storage.setItem('b',1)
+    // storage.setItem("user", { aa: 1 });
+    // storage.setItem('abcd', { aa: 2 }, 'user');
+    // storage.clear('b');
+    storage.clear('aa','user');
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
