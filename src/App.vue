c6b16f34e5296a5d4b5c7384569ee32abfd65268
yufei <feifei10244499@qq.com>
2020-01-07 17:50:41 +0800

    top

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,17 @@
 </template>
 
 <script>
-    // import storage from "./storage";
     export default {
         name: "app",
         components: {},
         data() {
-            return {
-                res: {}
-            };
         },
         mounted() {
-            //本地加载请求静态文件json文件的形式
-            // this.axios.get("/mock/user/login.json").then(res => {
-            //   this.res = res;
-            // });
-
-            //本地集成mockjs实现数据mock
-            this.axios.get("/user/login").then(res => {
-                this.res = res;
-            });
+            return {}
         }
     };
 </script>
 
 <style>
-    #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+    @import "./assets/scss/reset.scss";
 </style>
