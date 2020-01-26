fd6883cc6cd5d79235617b3026faefb35e375e82
yufei <feifei10244499@qq.com>
2020-01-26 16:08:55 +0800

    首页左侧菜单

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,42 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">手机 电话卡</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电话 盒子</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">笔记本 显示器 平板</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">家电 插线板</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">出行 穿戴</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">耳机 音箱</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电源 配件</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">生活 箱包</a>
+                            <div class="children"></div>
+                        </li>
+                    </ul>
+                </div>
                 <swiper :options="swiperOption">
                     <swiper-slide v-for="(item,index) in slideList" v-bind:key="index">
                         <a v-bind:href="'/#/product/'+item.id"><img v-bind:src="item.img"></a>
@@ -74,10 +110,51 @@
 </script>
 
 <style lang="scss">
+    @import "./../assets/scss/mixin";
+
     .index {
         .swiper-box {
+            .nav-menu {
+                position: absolute;
+                width: 264px;
+                height: 451px;
+                z-index: 2;
+                padding: 26px 0;
+                background-color: #55585a6e;
+                box-sizing: border-box;
+
+                .menu-wrap {
+                    .menu-item {
+                        height: 50px;
+                        line-height: 50px;
+
+                        a {
+                            position: relative;
+                            display:block;
+                            color: #ffffff;
+                            padding-left: 30px;
+                            font-size: 16px;
+
+                            &:after {
+                                position: absolute;
+                                right: 30px;
+                                top: 17.5px;
+                                content: ' ';
+                                @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+                            }
+                        }
+                        &:hover{
+                            background-color: #FF6600;
+                        }
+                    }
+                }
+            }
+
             .swiper-container {
                 height: 451px;
+                .swiper-button-prev{
+                    left: 274px;
+                }
 
                 img {
                     height: 100%;
